85684e4938f78490b2a85fc6cbcbe0997dfafbcf
apesteguia <mikeltxomikeltxo@gmail.com>
2024-11-18 20:39:55 +0100

    modificando lista y lectura

--- src/lectura.c
+++ src/lectura.c
@@ -1,4 +1,55 @@
 #include "lectura.h"
+#include "lista.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void leerDatos(ListaPersonas *, const char *) {}
-void leerPersonaPantalla(Persona *) {}
+static void leeFila(FILE *f, char *buf);
+static void guardaPersona(ListaPersonas *, const char *);
+
+void leerDatos(ListaPersonas *l, const char *ruta) {
+    FILE *f;
+    char buffer[TAM_BUFFER];
+
+    f = fopen(ruta, "r");
+    if (f == NULL) {
+        printf("ERROR: NO SE HA PODIDO ABRIR EL ARCHIVO EN LEER DATOS\n");
+        exit(1);
+    }
+
+    leeFila(f, buffer);
+    while (!feof(f)) {
+        leeFila(f, buffer);
+        if (strlen(buffer) > 30) {
+            guardaPersona(l, buffer);
+        }
+    }
+
+    fclose(f);
+}
+
+void leerPersonaPantalla(Persona *p) {
+    char buffer[TAM_BUFFER];
+
+    // pedir para cada atributo
+    printf("\nIntroduce el genero (Male/Female): ");
+    scanf("%255[^\n]", buffer);
+    if (!strcmp(buffer, "Male")) {
+        p->genero = 0.0;
+    } else {
+        p->genero = 1.0;
+    }
+}
+
+static void leeFila(FILE *f, char *buf) {
+    fscanf(f, "%[^\n]", buf);
+    fgetc(f);
+}
+
+static void guardaPersona(ListaPersonas *l, const char *buffer) {
+    Persona p;
+
+    // Partir la cadena en cada atributo insertar
+
+    insertar(l, p);
+}
--- src/lista.c
+++ src/lista.c
@@ -39,14 +39,52 @@ void nuevaLista(ListaPersonas *l, int k) {
     l->k = k;
     l->arrayVecinos = (Vecino *)malloc(sizeof(Vecino) * k);
     if (l->arrayVecinos == NULL) {
-        printf("ERROR ASIGNANDO MEMORIA PARA ARRAY DE VECINOS\n");
+        printf("ERROR: NO SE PUEDE ASIGNAR MEMORIA PARA ARRAY DE VECINOS\n");
         exit(1);
     }
 }
 
-void insertar(ListaPersonas *l, Persona p) {}
+void insertar(ListaPersonas *l, Persona p) {
+    NodoLista *nuevo;
+
+    nuevo = (NodoLista *)malloc(sizeof(NodoLista));
+    if (nuevo == NULL) {
+        printf("ERROR: NO SE PUEDE ASIGNAR MAS MEMORIA PARA INSERTAR\n");
+        return;
+    }
+    nuevo->p = p;
+    nuevo->sig = NULL;
+
+    if (esNulaLista(*l)) {
+        l->ini = nuevo;
+        l->fin = nuevo;
+    } else {
+        l->fin->sig = nuevo;
+        l->fin = nuevo;
+    }
+}
+
 bool esNulaLista(ListaPersonas l) { return (l.ini == NULL && l.fin == NULL); }
-void mostrarLista(ListaPersonas l) {}
+
+void mostrarLista(ListaPersonas l) {
+    NodoLista *aux;
+    int i;
+
+    if (esNulaLista(l)) {
+        printf("Lista vacia\n");
+        return;
+    }
+
+    i = 1;
+    aux = l.ini;
+    while (aux != NULL) {
+        // Hacer print de la persona
+
+        aux = aux->sig;
+        i++;
+    }
+}
+
 void calcularVecinosMasCercanos(ListaPersonas l, Persona p, int n) {}
 
 void vaciarLista(ListaPersonas *l) {
@@ -59,10 +97,11 @@ void vaciarLista(ListaPersonas *l) {
             idx = idx->sig;
             free(aux);
         }
-    }
 
-    l->ini = NULL;
-    l->fin = NULL;
+        l->ini = NULL;
+        l->fin = NULL;
+    }
 }
+
 // float calcularDistancia(Persona, Persona);
 // float calcularDistancia(Persona, Persona);
